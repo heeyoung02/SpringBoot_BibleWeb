56f726998b7e31d2ddecc5bb3bee0bdf75548f33
heeyoung <hhllldpei@gmail.com>
2023-07-10 08:51:56 +0900

    edit for qoddi

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -Dserver.port=$PORT $JAVA_OPTS -Dspring.profiles.active=prod -jar ./build/libs/bibletest-0.0.1-SNAPSHOT.jar
+java -Dserver.port=$PORT $JAVA_OPTS -Dspring.profiles.active=dev -jar ./build/libs/bibletest-0.0.1-SNAPSHOT.jar
